600ade33757c31d29c685b7badeb1c32ed27161c
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2023-06-08 12:28:45 -0300

    Added mamba to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /var/home/fabricio/mambaforge/bin/conda
+    eval /var/home/fabricio/mambaforge/bin/conda "shell.fish" "hook" $argv | source
+end
+
+if test -f "/var/home/fabricio/mambaforge/etc/fish/conf.d/mamba.fish"
+    source "/var/home/fabricio/mambaforge/etc/fish/conf.d/mamba.fish"
+end
+# <<< conda initialize <<<
+
