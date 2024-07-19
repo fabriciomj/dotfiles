c33aa829d4769c7ceb25a4985b0566e9715144b7
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2024-07-19 10:45:49 -0300

    Add fish shell config file

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,23 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+abbr -a -- dea deactivate
+abbr -a -- pya 'source .venv/bin/activate.fish'
+abbr -a -- less bat
+abbr -a -- cat 'bat --pager=never'
+abbr -a -- tree 'lsd --tree'
+abbr -a -- ls 'lsd --group-dirs first'
+abbr -a -- fdg 'fd .git -g -t d -u -E lazy'
+abbr -a -- syp 'systemctl poweroff'
+abbr -a -- syr 'systemctl reboot'
+abbr -a -- gtl 'git log'
+abbr -a -- gtds 'git diff --staged'
+abbr -a -- gtd 'git diff'
+abbr -a -- gtcm 'git commit -m'
+abbr -a -- gtc 'git commit'
+abbr -a -- gtpl 'git pull'
+abbr -a -- gtf 'git fetch'
+abbr -a -- gtps 'git push'
+abbr -a -- gts 'git status'
+abbr -a -- gtaa 'git add -A'
+abbr -a -- gta 'git add'
