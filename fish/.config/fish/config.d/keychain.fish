db2a7ac1ae74bc18aed01fa1e85ea179f0111a18
Stargazer3240 <112516550+fabriciomj@users.noreply.github.com>
2025-09-05 16:04:52 -0300

    Adiciona chave GPG no keychain do WSL

--- fish/.config/fish/config.d/keychain.fish
+++ fish/.config/fish/config.d/keychain.fish
@@ -1,5 +1,5 @@
 if status is-login
     and status is-interactive
     and set -q WSL_DISTRO_NAME
-    keychain --eval /home/fabricio/.ssh/id_ed25519 | source
+    keychain --eval --quiet -Q --noask 3CB29D135A367559 id_ed25519 | source 
 end
